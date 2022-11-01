c585378f6229c88e19d5e9616b88523b609cb7bd
bbyya551 <abe109yd576@gmail.com>
2022-11-02 00:18:01 +0900

    [Add]413

---
+++ kitasoftver2/practice413.java
@@ -0,0 +1,20 @@
+package kitasoftver2;
+
+public class practice413 {
+	public static void main(String[] args) {
+		int strike = 0;
+		int ball = 0;
+		while ((strike < 3) && (ball < 4)) {
+			System.out.println("ストライク=1 or ボール=2 or ファウル=3 ?");
+			int num = new java.util.Scanner(System.in).nextInt();
+			if (num == 1) {
+				strike += 1;
+			} else if (num == 2) {
+				ball += 1;
+			} else if (num == 3 && strike < 2) {
+				strike += 1;
+			}
+		}
+		System.out.println(ball + "ボール, " + strike + "ストライク");
+	}
+}
