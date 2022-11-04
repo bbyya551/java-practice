d0c23f866284806e445adc2d43cbc2f5157a40ab
bbyya551 <abe109yd576@gmail.com>
2022-11-04 21:31:06 +0900

    412

---
+++ kitasoftver2/practice412.java
@@ -0,0 +1,11 @@
+package kitasoftver2;
+
+public class practice412 {
+	public static void main(String[] args) {
+		int sum = 0;
+		while(sum <= 100) {
+			sum += new java.util.Scanner(System.in).nextInt();
+		}
+		System.out.println("合計は" + sum);
+	}
+}
